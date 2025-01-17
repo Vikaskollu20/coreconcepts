ecb496383044a0aa096bf85814824a11ef2f816c
Vikaskollu20 <vikaskollu20@gmail.com>
2025-01-17 17:41:47 +0530

    Looping Statement code changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Integer input=12345;
+        String value=input.toString();
+        int div=0;
+        for(int i=1;i<=value.length();i++) {
+            System.out.println(input % 10);
+            input= input/10;
+        }
+        System.out.println("------------------------------------------");
+        for(int i=0;i<=value.length()-1;i++) {
+            System.out.println(value.charAt(i));
+        }
+        System.out.println("------------------------------------------");
+
+        int count = 1;
+
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i && count <= 6; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
     }
 }
